25f96df42cbec33e7ae83215f4625cc53d22b75f
Zachary <zacharyflowers96@gmail.com>
2025-08-15 04:53:07 -0500

    update dc name

--- macro/launchCombine.sh
+++ macro/launchCombine.sh
@@ -1,9 +1,4 @@
-
-#dcdir=datacards
-#dcdir=datacards_22j
-#dcdir=datacards_11j
-#dcdir=datacards_2GLLL
-dcdir=datacards_eos
+dcdir=datacards_cascades
 pushd ../
 combineTool.py -M AsymptoticLimits  -d ${dcdir}/*/*.txt --there -n .limit --parallel 4
 combineTool.py -M Significance -t -1 --expectSignal=1 -d ${dcdir}/*/*.txt --there --parallel 4
