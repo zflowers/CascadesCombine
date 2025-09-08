5b13ebc1613373167ce3331d657b57ee443e647a
Zachary <zacharyflowers96@gmail.com>
2025-09-08 09:21:56 -0500

    change combine sys to 1%

--- src/BuildFit.cpp
+++ src/BuildFit.cpp
@@ -103,7 +103,7 @@ void BuildFit::BuildAsimovFit(JSONFactory* j, std::string signalPoint, std::stri
 	});
 
 	std::vector<std::string> binset = GetBinSet(j);
-	cb.cp().bin(binset).AddSyst(cb, "DummySys", "lnN", SystMap<>::init(1.10));
+	cb.cp().bin(binset).AddSyst(cb, "DummySys", "lnN", SystMap<>::init(1.01)); // 1%
       
 	//cb.PrintAll();
 	cb.WriteDatacard(datacard_dir+"/"+signalPoint+"/"+signalPoint+".txt");
