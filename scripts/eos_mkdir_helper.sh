f26b206788c743369f4b81d8e360b4c07f722f3c
Zachary <zacharyflowers96@gmail.com>
2025-12-06 10:00:32 -0600

    add eos helper

---
+++ scripts/eos_mkdir_helper.sh
@@ -0,0 +1,25 @@
+# Helper to quickly make new eos directories
+
+eosmkdir /store/user/lpcsusylep/NTUPLES_Cascades_v8/Summer20UL16APV_106X
+eosmkdir /store/user/lpcsusylep/NTUPLES_Cascades_v8/Summer20UL16APV_106X_Data
+eosmkdir /store/user/lpcsusylep/NTUPLES_Cascades_v8/Summer20UL16APV_106X_SMS
+eosmkdir /store/user/lpcsusylep/NTUPLES_Cascades_v8/Summer20UL16_106X
+eosmkdir /store/user/lpcsusylep/NTUPLES_Cascades_v8/Summer20UL16_106X_Data
+eosmkdir /store/user/lpcsusylep/NTUPLES_Cascades_v8/Summer20UL16_106X_SMS
+eosmkdir /store/user/lpcsusylep/NTUPLES_Cascades_v8/Summer20UL17_106X
+eosmkdir /store/user/lpcsusylep/NTUPLES_Cascades_v8/Summer20UL17_106X_Data
+eosmkdir /store/user/lpcsusylep/NTUPLES_Cascades_v8/Summer20UL17_106X_SMS
+eosmkdir /store/user/lpcsusylep/NTUPLES_Cascades_v8/Summer20UL18_106X
+eosmkdir /store/user/lpcsusylep/NTUPLES_Cascades_v8/Summer20UL18_106X_Data
+eosmkdir /store/user/lpcsusylep/NTUPLES_Cascades_v8/Summer20UL18_106X_SMS
+eosmkdir /store/user/lpcsusylep/NTUPLES_Cascades_v8/Summer22EE_130X
+eosmkdir /store/user/lpcsusylep/NTUPLES_Cascades_v8/Summer22EE_130X_Data
+eosmkdir /store/user/lpcsusylep/NTUPLES_Cascades_v8/Summer22_130X
+eosmkdir /store/user/lpcsusylep/NTUPLES_Cascades_v8/Summer22_130X_Cascades
+eosmkdir /store/user/lpcsusylep/NTUPLES_Cascades_v8/Summer22_130X_Data
+eosmkdir /store/user/lpcsusylep/NTUPLES_Cascades_v8/Summer22_130X_SMS
+eosmkdir /store/user/lpcsusylep/NTUPLES_Cascades_v8/Summer23BPix_130X
+eosmkdir /store/user/lpcsusylep/NTUPLES_Cascades_v8/Summer23BPix_130X_Cascades
+eosmkdir /store/user/lpcsusylep/NTUPLES_Cascades_v8/Summer23BPix_130X_Data
+eosmkdir /store/user/lpcsusylep/NTUPLES_Cascades_v8/Summer23_130X
+eosmkdir /store/user/lpcsusylep/NTUPLES_Cascades_v8/Summer23_130X_Data
